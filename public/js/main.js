055f4de66f71a52433e1496832ee544ed7507cd3
William Daigle <daiglewilliam13@gmail.com>
2022-03-02 12:13:30 -0800

    add xml parsing

--- public/js/main.js
+++ public/js/main.js
@@ -1,13 +1,25 @@
 console.log('connected');
+let scores;
 document.getElementById('file-loader')
     .addEventListener('change', function () {
 
         var fr = new FileReader();
         fr.onload = function () {
-            document.getElementById('output')
-                .textContent = fr.result;
-        }
+            scores=fr.result;
+            let parser = new DOMParser();
+            xml = parser.parseFromString(scores, "text/xml");
 
-        fr.readAsText(this.files[0]);
+            let songs = Array.from(xml.getElementsByTagName('Song'));
+            songs.forEach(song=>{
+                let songTitle = song.getAttribute('Dir').split('/')[2];
+                console.log(songTitle);
+                let p = document.createElement('p')
+                    .textContent = songTitle;
+                let output = document.getElementById('output')
+                    output.appendChild(p);
+            })
+
+        }
+       fr.readAsText(this.files[0]);
     })
 document.getElementById('upload-button');
