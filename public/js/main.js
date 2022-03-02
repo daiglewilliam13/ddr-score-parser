fce5858b05cbdbd7556227440a6c13a2b3b90711
William Daigle <daiglewilliam13@gmail.com>
2022-03-02 14:48:32 -0800

    update data parsing

--- public/js/main.js
+++ public/js/main.js
@@ -1,25 +1,25 @@
 console.log('connected');
 let scores;
-document.getElementById('file-loader')
-    .addEventListener('change', function () {
-
+document.getElementById('upload-button')
+    .addEventListener('click', function (e) {
+        e.preventDefault();
         var fr = new FileReader();
         fr.onload = function () {
             scores=fr.result;
             let parser = new DOMParser();
             xml = parser.parseFromString(scores, "text/xml");
-
+            console.log(xml)
+            let genData = xml.getElementsByTagName('DisplayName')[0];
             let songs = Array.from(xml.getElementsByTagName('Song'));
             songs.forEach(song=>{
                 let songTitle = song.getAttribute('Dir').split('/')[2];
-                console.log(songTitle);
-                let p = document.createElement('p')
-                    .textContent = songTitle;
+                let p = document.createElement('p');
+                    p.innerHTML = songTitle;
+                let difficulty = Array.from(song.getElementsByTagName('Steps'))[0]
+                console.log(difficulty?.getAttribute('Difficulty'))
                 let output = document.getElementById('output')
                     output.appendChild(p);
             })
-
         }
-       fr.readAsText(this.files[0]);
+       fr.readAsText(document.getElementById('file-loader').files[0]);
     })
-document.getElementById('upload-button');
